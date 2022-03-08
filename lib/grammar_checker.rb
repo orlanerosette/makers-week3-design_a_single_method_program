ca3a61509d3f415e4249ed922a7cf846a9ec59fb
Orlane <orlanedoumbe@gmail.com>
2022-03-08 17:16:39 +0000

    refactored exercise 2

--- lib/grammar_checker.rb
+++ lib/grammar_checker.rb
@@ -1,7 +1,7 @@
 def grammar_checker(text)
   fail "ERROR: an empty string is not a sentence!" if text.empty?
 
-  if text[0] == text[0].upcase && (text[-1] == '.' or text[-1] == '!' or text[-1] == '?')
+  if text[0] == text[0].upcase && (['.', '!', '?'].include? text[-1])
     return true
   else
     return false
